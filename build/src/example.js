9acda74a19dd9d612f02f8d219d7d42cd5a0128c
XuTian <xt@ekuaibao.com>
2017-09-05 13:39:43 +0800

    add jest

--- build/src/example.js
+++ build/src/example.js
@@ -2,8 +2,14 @@
  * Created by XuTian on 2017/9/5.
  */
 
-const sayHello = () => {
-  return 'hello'
+const sayHello = (word) => {
+  if (word === 'hello') {
+    return 'you said, hello'
+  } else if (word === 'world') {
+    return 'you said, world'
+  } else {
+    return word
+  }
 }
 
-export {sayHello}
+export { sayHello }
--- build/src/index.js
+++ build/src/index.js
@@ -2,6 +2,6 @@
  * Created by XuTian on 2017/9/5.
  */
 
-import {sayHello} from 'example'
+import { sayHello } from 'example'
 
-export {sayHello}
+export { sayHello }
